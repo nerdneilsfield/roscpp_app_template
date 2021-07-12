eebcb242e79b4c36e6fb508de900118d7a2ddf61
dengqi <dengqi@shanghaitech.edu.cn>
2021-07-12 19:24:55 +0800

    add a example msg and change the name

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include "ros/ros.h"
+#include "roscpp_app_template/hello.h"
+
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	std::cout << "Hello World" << std::endl;
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "helloworld");
+  ros::NodeHandle n;
+
+  ros::Publisher hello_pub =
+      n.advertise<roscpp_app_template::hello>("hello", 1000);
+
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+
+  while (ros::ok()) {
+    roscpp_app_template::hello hello;
+    hello.msg = "hello world!";
+    ROS_INFO("send");
+    hello_pub.publish(hello);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
 }
